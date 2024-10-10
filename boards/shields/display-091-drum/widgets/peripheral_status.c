6fd9d5018d76b29d585f97884f92829820b25b27
NoirGuo <88575961+NoirGuo@users.noreply.github.com>
2024-10-10 13:46:01 +0800

    右手屏幕跳舞猫

--- boards/shields/display-091-drum/widgets/peripheral_status.c
+++ boards/shields/display-091-drum/widgets/peripheral_status.c
@@ -267,7 +267,7 @@ int zmk_widget_status_init(struct zmk_widget_status *widget, lv_obj_t *parent) {
     lv_obj_center(animimg1); //居中
     // lv_animimg_set_src(animimg1, (lv_img_decoder_t**)my_anim_imgs,MAX_IMGS_NUM  );
     lv_animimg_set_src(animimg1, SRC(dance_images));
-    lv_animimg_set_duration(animimg1,1200);
+    lv_animimg_set_duration(animimg1,2400);
     lv_animimg_set_repeat_count(animimg1,LV_ANIM_REPEAT_INFINITE);  // count设置为LV_ANIM_REPEAT_INFINITE时表示一直循环显示
     lv_animimg_start(animimg1 );
 
